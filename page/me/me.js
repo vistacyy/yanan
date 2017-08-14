5cbc8dd0336c1488f5ba6fe51f0c5d2556e75bbc
vistacyy <vistacyy@163.com>
2017-08-15 00:17:20 +0800

    all

--- page/me/me.js
+++ page/me/me.js
@@ -5,62 +5,51 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    currentTab: '0',
+    userInfo: {},
+    category: [{
+        categoryid: '0',
+        name: '全部订单',
+      },
+      {
+        categoryid: '1',
+        name: '待付款',
+      },
+      {
+        categoryid: '2',
+        name: '待使用',
+      },
+      {
+        categoryid: '3',
+        name: '待评价',
+      },
+      {
+        categoryid: '4',
+        name: '退款',
+      }
+    ],
+  },
+  onLoad: function() {
+    // wx.login({
+    //   success: function(res) {
+    //     console.log('login=========>', res);
+    //   },
+    // });
+    var that = this;
+    wx.getUserInfo({
+      success: function(res) {
+        // console.log('getUserInfo=========>', res.userInfo);
+        that.setData({
+          userInfo: res.userInfo
+        });
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  switchTab: function(e) {
+    this.setData({
+      currentTab: e.currentTarget.dataset.categoryid
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
